8ed2a56aa5adb71b82f8c6bf7dff32a30267e37a
Nehaka-del <neha.a1@s.amity.edu>
2025-03-08 13:22:27 +0530

    started a switch case program in c

---
+++ Documents/source/switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+int day;
+printf("enter number (1-7):");
+scanf("%d", &day);
+
+switch(day) {
+case 1:
+printf("Sunday\n");
+break;
+case 2:
+printf("monday\n");
+break;
+case 3:
+printf("Tuesday\n");
+break;
